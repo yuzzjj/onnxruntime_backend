2b8ee8dd9e3aafff1d3498d8a15134c242759216
Baron <yyzzjj0515@yeah.net>
2021-09-29 21:55:22 +0800

    Update gen_ort_dockerfile.py

--- tools/gen_ort_dockerfile.py
+++ tools/gen_ort_dockerfile.py
@@ -391,7 +391,7 @@ if __name__ == '__main__':
     FLAGS = parser.parse_args()
     print("============================", FLAGS, "=============================")
     #add by yuzhenjiang
-    FLAGS.cudnn_home="/usr/local/cudnn-8.1/cuda"
+    FLAGS.cudnn_home="/usr/local/cudnn"
     print("============================", FLAGS, "=============================")
     if target_platform() == 'windows':
         # OpenVINO EP not yet supported for windows build
@@ -429,6 +429,7 @@ if __name__ == '__main__':
         dockerfile_for_windows(FLAGS.output)
 
     else:
+        print("1.=========================CUDNN_HOME==========================ENV：CUDNN_VERSION：", FLAGS.cuda_home， os.environ['CUDNN_VERSION'])
         if 'CUDNN_VERSION'in os.environ:
             version = None
             m = re.match(r'([0-9]\.[0-9])\.[0-9]\.[0-9]', os.environ['CUDNN_VERSION'])
@@ -436,7 +437,7 @@ if __name__ == '__main__':
                 version = m.group(1)
             if FLAGS.cudnn_home is None:
                 FLAGS.cudnn_home = '/usr/local/cudnn-{}/cuda'.format(version)
-
+        print("2.=========================CUDNN_HOME==========================", FLAGS.cuda_home)       
         if FLAGS.cuda_home is None:
             FLAGS.cuda_home = '/usr/local/cuda'
 
